77e2561f4bb47f184eda5e135fcf573ec09d576e
Ron Ganguly <c1ph3r9@gmail.com>
2019-01-22 17:00:03 -0600

    add users class and update user list event

--- public/js/chat.js
+++ public/js/chat.js
@@ -36,6 +36,11 @@ socket.on('disconnect', function () {
 
 socket.on('updateUserList',function(users){
   console.log('Users List', users)
+  let ol=jQuery('<ol></ol>')
+  users.forEach(function(user){
+    ol.append(jQuery('<li></li>').text(user))
+  })
+  jQuery("#users").html(ol)
 })
 
 
